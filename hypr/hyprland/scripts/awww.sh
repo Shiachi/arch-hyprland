3b7cde6961d70bc5c1a01cc7a1c480e6247c31f2
Raphiel <leroywlady@gmail.com>
2025-12-16 10:05:41 -0300

    Add random wallpaper change in base time day

--- hypr/hyprland/scripts/awww.sh
+++ hypr/hyprland/scripts/awww.sh
@@ -2,16 +2,43 @@
 
 primary_monitor="HDMI-A-1" 
 secondary_monitor="DP-1"
-
-wallpaper_1="/mnt/Files/Fan Art/Zpixiv/R/wall/Day/illust_86034756_20201201_1357_p0.jpg"
-
-wallpaper_2="/mnt/Files/Fan Art/Zpixiv/R/wall/Day/illust_45646550_20140828_1407_p0.jpg"
-
-
+base_wallpaper_dir="/mnt/Files/Fan Art/Zpixiv/R/wall"
 transition_args="--transition-type outer --transition-step 86 --transition-fps 75"
 
-awww img "$wallpaper_1" --outputs "$primary_monitor" $transition_args 
+while true; do
 
-matugen image -t scheme-content "$wallpaper_1"
+    current_hour=$(date +%H)
+    
+    wallpaper_subdir=""
 
-awww img "$wallpaper_2" --outputs "$secondary_monitor" $transition_args
+    case $current_hour in
+        06|07|08|09|10|11)
+            wallpaper_subdir="Morning"
+            ;;
+        12|13|14|15|16|17)
+            wallpaper_subdir="Day"
+            ;;
+        18|19|20)
+            wallpaper_subdir="Sunset"
+            ;;
+        *)
+            wallpaper_subdir="Night"
+            ;;
+    esac
+
+    current_wallpaper_dir="$base_wallpaper_dir/$wallpaper_subdir"
+
+    if [ -d "$current_wallpaper_dir" ]; then
+        wallpaper_1=$(find "$current_wallpaper_dir" -type f \( -name "*.jpg" -o -name "*.png" \) | shuf -n 1)
+        wallpaper_2=$(find "$current_wallpaper_dir" -type f \( -name "*.jpg" -o -name "*.png" \) | shuf -n 1)
+
+        awww img "$wallpaper_1" --outputs "$primary_monitor" $transition_args 
+        matugen image -t scheme-tonal-spot "$wallpaper_1"
+        awww img "$wallpaper_2" --outputs "$secondary_monitor" $transition_args
+    else
+        echo "Error: Directory $current_wallpaper_dir not found."
+    fi
+
+    sleep 1h
+
+done
