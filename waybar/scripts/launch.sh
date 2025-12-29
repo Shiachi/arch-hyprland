b839d2b9433826611833f6396de89d73ef4f5d86
Raphiel <leroywlady@gmail.com>
2025-12-29 20:19:33 -0300

    fix duplicate waybar instances

--- waybar/scripts/launch.sh
+++ waybar/scripts/launch.sh
@@ -2,12 +2,11 @@
 
 killall -q waybar
 
-while pgrep -x waybar >/dev/null; do sleep 1; done
+while pgrep -x waybar >/dev/null; do sleep 0.1; done
+hyprctl reload
 
 waybar -c "$HOME/.config/waybar/settings/config_primary.jsonc" \
        -s "$HOME/.config/waybar/themes/style_primary.css" &
 
 waybar -c "$HOME/.config/waybar/settings/config_secondary.jsonc" \
        -s "$HOME/.config/waybar/themes/style_secondary.css" &
-
-echo "Waybars launched..."
