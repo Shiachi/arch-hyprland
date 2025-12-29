d28b74d5841aa4b6218c6fa73e2be1d012268b38
Raphiel <leroywlady@gmail.com>
2025-12-29 01:08:57 -0300

    update script for launch waybar

--- waybar/scripts/launch.sh
+++ waybar/scripts/launch.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
 
-killall -9 waybar
-waybar &
+killall -q waybar
+
+while pgrep -x waybar >/dev/null; do sleep 1; done
+
+waybar -c "$HOME/.config/waybar/settings/config_primary.jsonc" \
+       -s "$HOME/.config/waybar/themes/style_primary.css" &
+
+waybar -c "$HOME/.config/waybar/settings/config_secondary.jsonc" \
+       -s "$HOME/.config/waybar/themes/style_secondary.css" &
+
+echo "Waybars launched..."
