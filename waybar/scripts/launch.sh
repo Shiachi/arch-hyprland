a86ad95f7fdf4947f6656ba234eb82b851951321
Raphiel <leroywlady@gmail.com>
2026-01-01 15:41:16 -0300

    hidden waybar debug

--- waybar/scripts/launch.sh
+++ waybar/scripts/launch.sh
@@ -6,7 +6,8 @@ while pgrep -x waybar >/dev/null; do sleep 0.1; done
 hyprctl reload
 
 waybar -c "$HOME/.config/waybar/settings/config_primary.jsonc" \
-       -s "$HOME/.config/waybar/themes/style_primary.css" &
+       -s "$HOME/.config/waybar/themes/style_primary.css" > /dev/null 2>&1 &
 
+# Launch Secondary Waybar (Silent)
 waybar -c "$HOME/.config/waybar/settings/config_secondary.jsonc" \
-       -s "$HOME/.config/waybar/themes/style_secondary.css" &
+       -s "$HOME/.config/waybar/themes/style_secondary.css" > /dev/null 2>&1 &
