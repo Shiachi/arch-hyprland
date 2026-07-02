41bdaf2a76f4696380f6c35db956287945a5eda3
Raphiel <leroywlady@gmail.com>
2026-07-01 22:07:38 -0400

    create temporary CUE file with only necessary info

--- hypr/hyprland/scripts/rename_music.sh
+++ hypr/hyprland/scripts/rename_music.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
+
+shopt -s nullglob
 
 echo --- Reading files ---
 unclean_cue=$(ls *.cue | grep -v '^clean' | head -n 1) 
 unsplit_flac=$(ls *.flac | grep -v '^[0-9]' | head -n 1) 
 
 echo --- $unclean_cue --- $unsplit_flac ---
+
+echo --- Cleaning cue ---
+grep -E "^\s*(FILE|TRACK|INDEX|TITLE|PERFORMER)" "$unclean_cue" | sed -E 's/:7[5-9]/:74/g' > clean_cue.cue
