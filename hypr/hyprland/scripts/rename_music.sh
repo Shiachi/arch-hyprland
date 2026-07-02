94463085d72a86e64bb05adfaa9a19b49d10da8c
Raphiel <leroywlady@gmail.com>
2026-07-02 17:20:17 -0400

    rename split tracks automatically using tags

--- hypr/hyprland/scripts/rename_music.sh
+++ hypr/hyprland/scripts/rename_music.sh
@@ -24,4 +24,24 @@ echo --- open picard for edit tags ---
 picard [0-9]*.flac -e CLUSTER -e LOOKUP all
 echo --- close picard ---
 
+echo --- rename split tracks ----
+for file in [0-9]*.flac; do
+  track_number=$(metaflac --show-tag=TRACKNUMBER "$file" | cut -d= -f2)
+  title=$(metaflac --show-tag=TITLE "$file" | cut -d= -f2)
+  disc=$(metaflac --show-tag=DISCNUMBER "$file" | cut -d= -f2)
+
+  if [ -z "$disc" ]; then disc="1"; fi
+
+  disc=$(printf "%02d" "$disc")
+  track_number=$(printf "%02d" "${track_number:-0}")
+
+  title=$(echo "$title" | tr '/' '-' | tr -d '?*:"<>|\\')
+
+  new_name="${disc}-${track_number}. ${title} .flac"
+  
+  if [ "$file" != "$new_name" ]; then
+    mv -v "$file" "$new_name"
+  fi
+done
+
 echo --- finish ----
