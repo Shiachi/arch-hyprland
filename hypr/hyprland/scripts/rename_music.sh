219231b550f0764b330f6ce0695fb021d2d8c401
Raphiel <leroywlady@gmail.com>
2026-07-01 22:22:58 -0400

    split audio tracks and apply tags using temporary CUE

--- hypr/hyprland/scripts/rename_music.sh
+++ hypr/hyprland/scripts/rename_music.sh
@@ -10,3 +10,12 @@ echo --- $unclean_cue --- $unsplit_flac ---
 
 echo --- Cleaning cue ---
 grep -E "^\s*(FILE|TRACK|INDEX|TITLE|PERFORMER)" "$unclean_cue" | sed -E 's/:7[5-9]/:74/g' > clean_cue.cue
+
+echo "--- Split tracks ---"
+shnsplit -f clean_cue.cue -o flac -t "%n" "$unsplit_flac"
+
+echo "--- Add tag from clean cue to split tracks ---"
+cuetag.sh clean_cue.cue [0-9]*.flac  
+
+echo ---delete clean cue file ---
+rm clean_cue.cue
